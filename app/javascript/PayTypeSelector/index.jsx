b9117626e9584e48f16a6db4a4369907e73f4b75
Artafarin <artafarinn@gmail.com>
2022-09-01 15:31:10 -0500

    dynamic payment type

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -32,7 +32,7 @@ class PayTypeSelector extends React.Component {
           <select id="order_pay_type" onChange={this.onPayTypeSelected} 
             name="order[pay_type]">
             <option value="">Select a payment method</option>
-            <option value="Check">Checkk</option>
+            <option value="Check">Check</option>
             <option value="Credit card">Credit card</option>
             <option value="Purchase order">Purchase order</option>
           </select>
@@ -43,3 +43,4 @@ class PayTypeSelector extends React.Component {
   }
 }
 export default PayTypeSelector
+
